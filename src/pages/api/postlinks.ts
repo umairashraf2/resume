fdccc0187b2b3c5b975ee6204f9793ddf6b7d36c
Umair Ashraf <umair.dco@gmail.com>
2023-06-26 21:46:56 +0500

    fixed

--- src/pages/api/postlinks.ts
+++ src/pages/api/postlinks.ts
@@ -1,19 +1,19 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-// const fs = require('fs');
-// import path from 'path';
+const fs = require('fs');
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const {method} = req;
 
   switch (method) {
-    case 'GET':
-      // const userData = req.body;
-      // const data = JSON.stringify(userData, null, 2);
-      // fs.writeFileSync(path.join(process.cwd(), 'src/data/socialLinks.json'), data, 'utf-8');
+    case "POST":
+      const userData = req.body;
+      const data = JSON.stringify(userData, null, 2);
+      fs.writeFileSync(path.join(process.cwd(), 'src/data/socialLinks.json'), data, 'utf-8');
       res.status(200).json({message: 'Data updated successfully'});
       return;
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['POST']);
       res.status(405).json({message: 'Data not updated successfully', method});
       return;
 
