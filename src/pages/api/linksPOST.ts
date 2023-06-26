5453600f5a7b043547616ebc43a5eb2f1d8ee0c6
Umair Ashraf <umair.dco@gmail.com>
2023-06-26 20:37:22 +0500

    fixed

--- src/pages/api/linksPOST.ts
+++ src/pages/api/linksPOST.ts
@@ -3,13 +3,17 @@ const fs = require('fs');
 import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const userData = req.body;
-    const data = JSON.stringify(userData, null, 2);
-    fs.writeFileSync(path.join(process.cwd(), 'src/data/socialLinks.json'), data, 'utf-8');
-    res.status(200).json({message: 'Data updated successfully'});
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  const {method} = req;
+
+  switch (method) {
+    case 'POST':
+      const userData = req.body;
+      const data = JSON.stringify(userData, null, 2);
+      fs.writeFileSync(path.join(process.cwd(), 'src/data/socialLinks.json'), data, 'utf-8');
+      res.status(200).json({message: 'Data updated successfully'});
+      return;
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
